048f269601567ee249f1b6c733f3b85dcc19c3d8
Shameer29 <54391497+Shameer29@users.noreply.github.com>
2020-11-28 10:40:16 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router();
 const firebase = require('../config/firebase-config-client');
 
-/* GET home page. */
-router.get('/', (req, res) => {
-  let unsubscribe = firebase.auth().onAuthStateChanged(user => {
-    res.render('index', { user: user });
-  });
 
-  unsubscribe();
-});
 
 /* DELETE home page (logout).*/
 router.post('/logout', (req, res) => {
